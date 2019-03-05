3caf6ee862826da423d998c2d2bbcc8fadbeaab6
Utz Stauder <utzstauder@users.noreply.github.com>
2019-03-05 13:27:36 +0100

    Implemented waypoint-based Enemy input

--- Assets/Scripts/Gameplay/MovementInput/EnemyMovementInput.cs
+++ Assets/Scripts/Gameplay/MovementInput/EnemyMovementInput.cs
@@ -7,19 +7,65 @@ public class EnemyMovementInput : MonoBehaviour, IMovementInput
     {
         get
         {
-            throw new System.NotImplementedException();
+            return inputVector;
         }
     }
 
-    // Use this for initialization
-    void Start()
-    {
+    Vector2 inputVector = new Vector2();
+    Vector3 direction;
 
+    [SerializeField] Transform[] waypoints;
+
+    int currentWaypointIndex = 0;
+    int CurrentWaypointIndex
+    {
+        get { return currentWaypointIndex; }
+        set
+        {
+            if (value != currentWaypointIndex)
+            {
+                if (value < 0)
+                {
+                    value = waypoints.Length - 1;
+                } else if (value >= waypoints.Length)
+                {
+                    value = 0;
+                }
+                currentWaypointIndex = value;
+            }
+        }
+    }
+
+    [SerializeField] float distanceToTarget = 0.05f;
+
+    Vector3 TargetPosition {
+        get {
+            if (waypoints == null) return transform.position;
+            if (waypoints.Length < 1) return transform.position;
+            return waypoints[CurrentWaypointIndex].position;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (ArrivedAtTargetPosition())
+        {
+            // select next waypoint
+            CurrentWaypointIndex += 1;
+            // CurrentWaypointIndex = Random.Range(0, waypoints.Length);
+        }
 
+        direction = TargetPosition - transform.position;
+
+        inputVector.x = direction.x;
+        inputVector.y = direction.z;
+
+        inputVector.Normalize();
+    }
+
+    bool ArrivedAtTargetPosition()
+    {
+        return (Vector3.Distance(transform.position, TargetPosition) <= distanceToTarget);
     }
 }
